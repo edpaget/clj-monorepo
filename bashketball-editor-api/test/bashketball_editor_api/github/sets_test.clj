d98693108e4fff4a07fdd87d0c55a45b8a7c5119
Edward Paget <ed.paget@gmail.com>
2025-11-26 14:11:02 -0600

    Delete some stuff

--- bashketball-editor-api/test/bashketball_editor_api/github/sets_test.clj
+++
@@ -1,8 +0,0 @@
-(ns bashketball-editor-api.github.sets-test
-  "Legacy test namespace - see bashketball-editor-api.git.sets-test for current tests."
-  (:require
-   [clojure.test :refer [deftest is testing]]))
-
-(deftest placeholder-test
-  (testing "legacy namespace placeholder"
-    (is true "See bashketball-editor-api.git.sets-test for set repository tests")))
